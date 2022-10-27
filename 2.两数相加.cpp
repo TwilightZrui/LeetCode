a34062f15ed0ab33c2ee6a29653f29733cc8749a
TwilightZrui <1193882630@qq.com>
2022-10-27 20:45:44 +0800

    conconvexHull

---
+++ 2.两数相加.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/*
+先定义一个新链表
+从l1和l2的头开始遍历，相加得sum
+
+*/
+class Solution {
+  public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *dummy = new ListNode(0);
+        }
+};
+// @lc code=end
---
+++ convexHull.cpp
@@ -0,0 +1,50 @@
+/***
+ * @Author       : TwilightZrui
+ * @Date         : 2022-10-27 16:26:19
+ * @LastEditors  : TwilightZrui
+ * @LastEditTime : 2022-10-27 16:26:20
+ * @FilePath     : /LeetCode/convexHull.cpp
+ * @Description  :
+ * @Copyright (c) 2022, All Rights Reserved.
+ */
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <math.h>
+#include <stack>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+    //随机生成测试点集
+    Mat img = Mat::zeros(500, 500, CV_8UC3);
+    RNG rng(getTickCount());
+    vector<Point2f> points;
+    for (int i = 0; i < 50; i++) {
+        Point2f point;
+        point.x = rng.uniform(cvRound(img.cols / 4), cvRound(img.cols * 3 / 4));
+        point.y = rng.uniform(cvRound(img.rows / 4), cvRound(img.rows * 3 / 4));
+        circle(img, point, 2,
+               Scalar(rng.uniform(0, 255), rng.uniform(0, 255),
+                      rng.uniform(0, 255)),
+               FILLED, LINE_AA);
+        points.push_back(point);
+    }
+
+    //创建一个容器存放函数返回的凸包顶点
+    vector<Point2f> Hull;
+    //调用凸包函数
+    convexHull(points, Hull, false);
+
+    //凸包绘制
+    for (int i = 0; i < Hull.size(); i++) {
+        line(img, Hull[i], Hull[(i + 1) % Hull.size()], Scalar(255, 0, 0), 2,
+             LINE_AA);
+    }
+
+    namedWindow("Convex", WINDOW_AUTOSIZE);
+    imshow("Convex", img);
+    waitKey(0);
+}
